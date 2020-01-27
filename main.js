18377581886555647f652d8b86f72dfde7874452
Lachlan Blake <lachlan.blake10@gmail.com>
2020-01-27 16:46:36 +1100

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const client = platformClient.ApiClient.instance;
 client.setEnvironment(platformClient.PureCloudRegionHosts.ap_southeast_2);
 
 const clientId = 'c09753f7-587a-43b8-875e-f060c8dd3272';
-const redirectUri = 'https://djlh1c0cndn6z.cloudfront.net/';
+const redirectUri = 'https://otimie.github.io/webrtc-test/index.html';
 
 client.loginImplicitGrant(clientId, redirectUri).then(() => {
 	const sdk = new window.PureCloudWebrtcSdk({
