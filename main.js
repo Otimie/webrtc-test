f52dd753b0b7ba8c137606a5e95e2c94d219f692
Lachlan Blake <lachlan.blake10@gmail.com>
2020-01-27 16:42:32 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+const platformClient = require('platformClient');
 
+const client = platformClient.ApiClient.instance;
+
+client.setEnvironment(platformClient.PureCloudRegionHosts.ap_southeast_2);
+
+const clientId = 'c09753f7-587a-43b8-875e-f060c8dd3272';
+const redirectUri = 'https://djlh1c0cndn6z.cloudfront.net/';
+
+client.loginImplicitGrant(clientId, redirectUri).then(() => {
+	const sdk = new window.PureCloudWebrtcSdk({
+		accessToken: client.authData.accessToken,
+		environment: 'mypurecloud.com.au'
+	});
+	sdk.initialize();
+});
